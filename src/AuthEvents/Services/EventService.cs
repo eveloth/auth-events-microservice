ebfb7fd05561ba7e0fc46eb505994351538feaad
Eveloth <vadim.stepanov.web@gmail.com>
2023-03-23 17:01:35 +0300

    chore: add event service

---
+++ src/AuthEvents/Services/EventService.cs
@@ -0,0 +1,45 @@
+using AuthEvents.Contracts.Queries;
+using AuthEvents.Data;
+using AuthEvents.Data.Filters;
+using AuthEvents.Data.Repository;
+using AuthEvents.Domain;
+using Mapster;
+using MapsterMapper;
+
+namespace AuthEvents.Services;
+
+public class EventService : IEventService
+{
+    private readonly IEventRepository _eventRepository;
+    private readonly IMapper _mapper;
+
+    public EventService(IEventRepository eventRepository, IMapper mapper)
+    {
+        _eventRepository = eventRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<IEnumerable<Event>> Get(
+        CancellationToken ct,
+        TimeFilterQuery? timeFilterQuery = null,
+        EventFilterQuery? eventFilterQuery = null,
+        PaginationQuery? paginationQuery = null
+    )
+    {
+        var timeFilter = timeFilterQuery.Adapt<TimeFilterQuery?, TimeFilter?>();
+        var eventFilter = eventFilterQuery.Adapt<EventFilterQuery?, EventFilter?>();
+        var paginationFilter = paginationQuery.Adapt<PaginationQuery?, PaginationFilter?>();
+
+        var result = await _eventRepository.Get(ct, timeFilter, eventFilter, paginationFilter);
+
+        var events = _mapper.Map<IEnumerable<Event>>(result);
+        return events;
+    }
+
+    public async Task<long> Add(Event newEvent, CancellationToken ct)
+    {
+        var eventModel = _mapper.Map<EventModel>(newEvent);
+
+        return await _eventRepository.Add(eventModel, ct);
+    }
+}
---
+++ src/AuthEvents/Services/IEventService.cs
@@ -0,0 +1,12 @@
+using AuthEvents.Contracts.Queries;
+using AuthEvents.Domain;
+
+namespace AuthEvents.Services;
+
+public interface IEventService
+{
+    Task<IEnumerable<Event>> Get(CancellationToken ct, TimeFilterQuery? timeFilterQuery = null,
+        EventFilterQuery? eventFilterQuery = null, PaginationQuery? paginationQuery = null);
+
+    Task<long> Add(Event newEvent, CancellationToken ct);
+}
